95cb4b87bff7700f2646879afc75e1592f1382db
Blake Scott <blakerscott@gmail.com>
2016-04-01 11:55:59 -0700

    add ability to toggle show/hide of details

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  areDetailsShowing: false,
+    actions: {
+      detailsShow: function() {
+        this.set('areDetailsShowing', true);
+      },
+      detailsHide: function() {
+        this.set('areDetailsShowing', false);
+      }
+    }
 });
