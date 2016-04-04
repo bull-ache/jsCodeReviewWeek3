a28a5f75ceb234b02b8bd1882612000436ee07ec
Blake Scott <blakerscott@gmail.com>
2016-04-03 21:42:03 -0700

    update new-question component source file

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
+
 export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion' true);
+    },
+
+    save1() {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        notes: this.get('notes'),
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('save2', params);
+      }
+  }
 });
