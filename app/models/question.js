2fe30260a4b5b73c7a8bd3c13289685cb501b8a7
Blake Scott <blakerscott@gmail.com>
2016-04-03 21:14:27 -0700

    update models

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   content: DS.attr(),
   author: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
+  answers: DS.hasMany('rental', {async: true })
 });
