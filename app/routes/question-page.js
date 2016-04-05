c1737ea98e40a87ab59c7d0fc619404a8c7a86a4
Blake Scott <blakerscott@gmail.com>
2016-04-04 21:20:26 -0700

    create question page route

---
+++ app/routes/question-page.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+});
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    save3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      console.log(params);
+      var question = params.question;
+      category.get('answer').addObject(newAnswer);
+      newAnswer.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question');
+      }
+    }
+  });
